c9949d8db541bd47fec65973d47328194e3cac4b
Mads Akselsen <akselsenmads@gmail.com>
2023-05-29 12:09:23 +0200

    styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import './globals.css'
-import { Inter, Roboto_Slab } from 'next/font/google'
+import "./globals.css";
+import { Inter, Roboto_Slab } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const roboto = Roboto_Slab({
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-roboto-slab',
-  display: 'swap',
+	weight: "700",
+	subsets: ["latin"],
+	variable: "--font-roboto-slab",
+	display: "swap",
 });
 
 export const metadata = {
-  title: 'Netflix Clone',
-  description: 'Dicover movies',
-}
+	title: "Netflix Clone",
+	description: "Dicover movies",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={roboto.className}>{children}</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-
-import styles from './page.module.css'
+import Banner from "../components/banner/banner";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Netflix</h1>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<h1>Netflix</h1>
+
+			{/* <NavBar /> */}
+			<Banner
+				title="Clifford the red dog"
+				subTitle="a very cute dog"
+				imgUrl="/static/clifford.webp"
+			/>
+			{/* <Card /> */}
+		</div>
+	);
 }
