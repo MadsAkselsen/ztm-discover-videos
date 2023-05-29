379c3aab56d1d58bea1bf6db2db7e838cc0f22d3
Mads Akselsen <akselsenmads@gmail.com>
2023-05-29 15:19:16 +0200

    fallback image

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import styles from "./card.module.css";
+import { useState } from "react";
 
 interface BannerProps {
 	imgUrl: string;
@@ -12,15 +14,27 @@ const classMap = {
 	small: styles.smItem,
 };
 
-const Card: React.FC<BannerProps> = ({ imgUrl, size }) => {
+const Card: React.FC<BannerProps> = ({
+	imgUrl = "/static/clifford.webp",
+	size = "medium",
+}) => {
+	const [imgSrc, setImgSrc] = useState(imgUrl);
+
+	// fallback image if there is an error in the image src
+	const handleOnError = () => {
+		console.log("hii error");
+		setImgSrc("/static/clifford.webp");
+	};
+
 	return (
 		<div className={styles.container}>
 			Card
 			<div className={classMap[size]}>
 				<Image
-					src={imgUrl}
+					src={imgSrc}
 					alt="image"
 					fill={true}
+					onError={handleOnError}
 					className={styles.cardImg}
 				/>
 			</div>
