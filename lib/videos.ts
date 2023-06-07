0682d320f2cc059c4d2478a92d382af35cdac6d4
Mads Akselsen <akselsenmads@gmail.com>
2023-06-07 08:33:07 +0200

    added youtube api

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,7 +1,15 @@
 import videoData from "../data/videos.json";
 
 export const getVideos = async (): Promise<Video[]> => {
-  return videoData.items.map((item: VideoApiResp) => {
+  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+
+  const response = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${YOUTUBE_API_KEY}`
+  );
+
+  const data: YoutubeSearchListResp = await response.json();
+
+  return data.items.map((item) => {
     return {
       title: item.snippet.title,
       imgUrl: item.snippet.thumbnails.high.url,
@@ -9,6 +17,18 @@ export const getVideos = async (): Promise<Video[]> => {
     }
   });
 };
+
+interface YoutubeSearchListResp {
+  kind: string,
+  etag: string,
+  nextPageToken: string,
+  regionCode: string,
+  pageInfo: {
+    totalResults: number,
+    resultsPerPage: number
+  },
+  items: VideoApiResp[]
+}
 
 export interface Video {
   title: string;
