9cf78bdee877d91b6c7ce2959d1a9264f39d3566
Mads Akselsen <akselsenmads@gmail.com>
2023-06-24 12:09:20 +0200

    implement popular videos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Card from "@/components/card/card";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import styles from "./page.module.css";
 import SectionCards from "@/components/card/section-cards";
-import { Video } from "@/lib/videos";
 
-import { getVideos } from "../lib/videos";
+import { getPopularVideos, getVideos } from "../lib/videos";
 // import { GetServerSidePropsContext } from "next";
 
 // interface HomeProps {
@@ -24,38 +22,40 @@ export default async function Home() {
 	const disneyVideos = await getVideos("disney trailer");
 	const productivityVideos = await getVideos("productivity");
 	const travelVideos = await getVideos("travel");
-	const popularVideos = await getVideos("popular");
-	console.log({ disneyVideos });
-	
+	const popularVideos = await getPopularVideos();
+
 	return (
 		<div className={styles.container}>
-			<NavBar username="akselsenmads@gmail.com" />
-			<Banner
-				title="Clifford the red dog"
-				subTitle="a very cute dog"
-				imgUrl="/static/clifford.webp"
-			/>
-			<div className={styles.sectionWrapper}>
-				<SectionCards
-					title="Disney"
-					videos={disneyVideos}
-					size="large"
+			<div className={styles.main}>
+				<NavBar username="ankita@ank.com" />
+				<Banner
+					title="Clifford the red dog"
+					subTitle="a very cute dog"
+					imgUrl="/static/clifford.webp"
 				/>
-				<SectionCards
-					title="Travel"
-					videos={travelVideos}
-					size="small"
-				/>
-				<SectionCards
-					title="Productivity"
-					videos={productivityVideos}
-					size="medium"
-				/>
-				<SectionCards
-					title="Popular"
-					videos={popularVideos}
-					size="small"
-				/>
+
+				<div className={styles.sectionWrapper}>
+					<SectionCards
+						title="Disney"
+						videos={disneyVideos}
+						size="large"
+					/>
+					<SectionCards
+						title="Travel"
+						videos={travelVideos}
+						size="small"
+					/>
+					<SectionCards
+						title="Productivity"
+						videos={productivityVideos}
+						size="medium"
+					/>
+					<SectionCards
+						title="Popular"
+						videos={popularVideos}
+						size="small"
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- lib/videos.ts
+++ lib/videos.ts
@@ -1,70 +1,101 @@
+export const getCommonVideos = async (url: string): Promise<Video[]> => {
+	const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
-export const getVideos = async (searchQuery: string): Promise<Video[]> => {
-  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+	try {
+		const BASE_URL = "youtube.googleapis.com/youtube/v3";
+		const response = await fetch(
+			`https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`
+		);
 
-  const response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&q=${searchQuery}%20trailer&key=${YOUTUBE_API_KEY}`
-  );
+		const data: YoutubeSearchListResp = await response.json();
+		if (data?.error) {
+			console.error("Youtube API error", data.error);
+			return [];
+		}
 
-  const data: YoutubeSearchListResp = await response.json();
-
-  return data.items.map((item) => {
-    return {
-      title: item.snippet.title,
-      imgUrl: item.snippet.thumbnails.high.url,
-      id: item.id.videoId
-    }
-  });
+		return data.items.map((item) => {
+			const id = processId(item);
+			return {
+				title: item.snippet.title,
+				imgUrl: item.snippet.thumbnails.high.url,
+				id,
+			};
+		});
+	} catch (error) {
+		console.error("Something went wrong with video library", error);
+		return [];
+	}
 };
 
+export const getVideos = (searchQuery: string) => {
+	const URL = `search?part=snippet&q=${searchQuery}&type=video`;
+	return getCommonVideos(URL);
+};
+
+export const getPopularVideos = () => {
+	const URL =
+		"videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US";
+	return getCommonVideos(URL);
+};
+
+function processId(item: VideoApiResp): string {
+	if (typeof item.id === "string") {
+		// Handle the case where the id is a string
+		return item.id;
+	} else {
+		// Handle the case where the id is of type 'Id'
+		return item.id.videoId;
+	}
+}
+
 interface YoutubeSearchListResp {
-  kind: string,
-  etag: string,
-  nextPageToken: string,
-  regionCode: string,
-  pageInfo: {
-    totalResults: number,
-    resultsPerPage: number
-  },
-  items: VideoApiResp[]
+	kind: string;
+	etag: string;
+	nextPageToken: string;
+	regionCode: string;
+	pageInfo: {
+		totalResults: number;
+		resultsPerPage: number;
+	};
+	items: VideoApiResp[];
+	error: string;
 }
 
 export interface Video {
-  title: string;
-  imgUrl: string;
-  id: string;
+	title: string;
+	imgUrl: string;
+	id: string;
 }
 
 interface VideoApiResp {
-  kind: string;
-  etag: string;
-  id: Id;
-  snippet: Snippet;
+	kind: string;
+	etag: string;
+	id: Id | string;
+	snippet: Snippet;
 }
 
 interface Thumbnail {
-    url: string;
-    width: number;
-    height: number;
-  }
-  
-  interface Snippet {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: {
-      default: Thumbnail;
-      medium: Thumbnail;
-      high: Thumbnail;
-    };
-    channelTitle: string;
-    liveBroadcastContent: string;
-    publishTime: string;
-  }
-  
-  interface Id {
-    kind: string;
-    videoId: string;
-  } 
-  
+	url: string;
+	width: number;
+	height: number;
+}
+
+interface Snippet {
+	publishedAt: string;
+	channelId: string;
+	title: string;
+	description: string;
+	thumbnails: {
+		default: Thumbnail;
+		medium: Thumbnail;
+		high: Thumbnail;
+	};
+	channelTitle: string;
+	liveBroadcastContent: string;
+	publishTime: string;
+}
+
+interface Id {
+	kind: string;
+	videoId: string;
+}
